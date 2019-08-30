0106bd4d5a62db3aa0c77af239d5730bb9afc731
Anthony Oglesby <ogle1ant@gmail.com>
2019-08-30 14:42:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,12 @@
-def my_collect(array)
-  i = 0
-  collect = []
-  while i < array.length
-    collect << yield(array[i])
-    i+=1
-  end
-  collect
-end
+# def my_collect(array)
+#   i = 0
+#   collect = []
+#   while i < array.length
+#     collect << yield(array[i])
+#     i+=1
+#   end
+#   collect
+# end
 
 def my_collection(array)
   i = 0
